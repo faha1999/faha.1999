0805c27b0777a3b40c825d13b851d44cbf24ed97
faha1999 <kafahad1999@gmail.com>
2022-02-19 23:40:06 +0600

    qualification function test failed

--- app/js/main.js
+++ app/js/main.js
@@ -44,3 +44,23 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener('click', toggleSkills);
 });
+
+// start qualification
+const tabs = document.querySelectorAll('[data-content]'),
+  tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('qualification_active');
+    });
+    target.classList.add('qualification_active');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification_active');
+    });
+    tab.classList.add('qualification_active');
+  });
+});
